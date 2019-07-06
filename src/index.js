bc42b5143e0846d773a77132c3a554e23dbf3951
Damhan Richardson <damhanrichardson@Gmail.com>
2019-07-06 11:54:48 +0100

    Added template components & a sample note

--- src/index.js
+++ src/index.js
@@ -2,8 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class NoteRow extends React.Component {
+    render() {
+        return (
+            <b className="noteRow">
+                SampleNote
+            </b>
+        );
+    }
+}
+
+class DeleteNoteBut extends React.Component {
+    render() {
+        return;
+    }
+}
+
+class Notes extends React.Component {
+    render() {
+        return;
+    }
+}
+
+class InsertBar extends React.Component {
+    render() {
+        return; 
+    }
+}
+
 class NoteApplication extends React.Component {
     render() {
         return;
     }
 }
+
+ReactDOM.render(
+    <NoteRow />,
+    document.getElementById('root')
+  );
